3af2d372020acadbaeca48f5dc972fd5c86bf5d1
Andre Borge <andreborge1008@gmail.com>
2026-08-21 23:32:33 +0200

    add: virt-manager setup

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
 
   users.users.andre = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; 
+    extraGroups = [ "wheel" "libvirtd" ]; 
   };
   ## System Packages
   environment.systemPackages = [
@@ -110,6 +110,9 @@ programs.sway = {
   enable = true;
   wrapperFeatures.gtk = true;
 };
+virtualisation.libvirtd.enable = true;
+programs.virt-manager.enable = true;
+
 programs.firefox.enable = true;
 programs.xwayland.enable = true;
 programs.nix-ld.enable = true;
